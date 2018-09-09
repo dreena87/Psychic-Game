aa17317dd11d3007cdbd35d294eccb42994a1e2c
Adreena Howard <adreena.howard@yahoo.com>
2018-09-08 20:10:08 -0500

    updated html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
-//initialize global vars to game
-var guessArr = [];  //guess array
+
+var guessArr = [];  
 var wins =0;
 var losses =0;
 var totalGuesses = 10; 
@@ -10,59 +10,58 @@ var displayLosses = document.getElementById("losses");
 var displayGuesses = document.getElementById("guesses");
 var guessSoFar = document.getElementById("guessSoFar");
 
-//generate computer guess
+
 var charset = "abcdefghijklmnopqrstuvwxyz";
 computerGuess += charset.charAt(Math.floor(Math.random() * charset.length));
-//end computer guess
 
 document.onkeyup = function(event){
 
-    var key = event.key;    //capture key pressed by user
+    var key = event.key;    
     var bypass;
 
-    //error checking block + input conditioning
-    if(charset.indexOf(key) === -1)  //is the entry valid?
+    
+    if(charset.indexOf(key) === -1)  
     {
         bypass=1
         alert("Please select an english character");
     }
-    else if(guessArr.indexOf(key) !== -1)    //did the user already guess the entered character?
+    else if(guessArr.indexOf(key) !== -1)    
     {
         bypass=1;
         alert("You have already guessed " + key + ". Please guess another character."); 
     }
-    else //user guessed something new that wasn't guessed before
+    else
     {
         key.toLowerCase();
-        guessArr.push(key); //add user guess to array of used guesses
+        guessArr.push(key); 
     }
 
-    //end error checkng block +input conditioning
+    
 
-    //Logic for game
+   
     if(!bypass){
-        //did we guess correctly?
-        if(computerGuess === key ) //did the user guess it
+        
+        if(computerGuess === key ) 
         {
             alert("You did it! The computer's guess was indeed " + computerGuess + ".");
-            wins++; //increment wins counter
-            totalGuesses=10;    //reset guess counter
-            computerGuess = guessArr = []; //clear arrays
-            computerGuess += charset.charAt(Math.floor(Math.random() * charset.length));    //generate new guess
+            wins++; 
+            totalGuesses=10;    
+            computerGuess = guessArr = []; 
+            computerGuess += charset.charAt(Math.floor(Math.random() * charset.length));    
         }
-        else if (computerGuess !== key && totalGuesses ===1) //did we fail and run out of guesses
+        else if (computerGuess !== key && totalGuesses ===1) 
         {
             losses++;
             totalGuesses=10;
             alert("The computer's guess was " + computerGuess + ".");
-            computerGuess = guessArr = []; //clear arrays
-            computerGuess += charset.charAt(Math.floor(Math.random() * charset.length));    //generate new guess
+            computerGuess = guessArr = []; 
+            computerGuess += charset.charAt(Math.floor(Math.random() * charset.length));   
         }
-        else{   //didn't guess but have guesses left
+        else{   
             totalGuesses--;
         }
 
-        //end code logic
+        
         displayWins.textContent = "Wins: " + wins;
         displayLosses.textContent = "Losses: " + losses;
         displayGuesses.textContent = "Guesses Left: " + totalGuesses;
